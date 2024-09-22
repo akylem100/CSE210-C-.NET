13ffedacf2afb37f224a4acd01fcdf5c5dbafdc9
akylem100 <akylem@byui.edu>
2024-09-21 18:17:00 -0600

    completed prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        Console.Write("Guess the magic number between 1-100! What is your guess? ");
+        string user_guess = Console.ReadLine();
+        int guess = int.Parse(user_guess);
+
+        while (guess != magic_number)
+        {
+            if (guess > magic_number)
+            {
+                Console.WriteLine("It's lower than that!");
+            }
+            else
+            {
+                Console.WriteLine("It's higher than that!");
+            }
+            Console.Write("Try again! What is your next guess? ");
+            user_guess = Console.ReadLine();
+            guess = int.Parse(user_guess);
+        }
+
+        if (guess == magic_number)
+        {
+            Console.WriteLine("You guessed it! Great job!");
+        }
     }
 }
