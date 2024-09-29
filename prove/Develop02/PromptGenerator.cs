b0c5581b1cd5e3bf3e2887d49e6c20cfa679c5e6
akylem100 <akylem@byui.edu>
2024-09-28 23:48:09 -0600

    added more prompts to finished journal

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -18,9 +18,15 @@ public class PromptGenerator
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
+            "If I had one thing I could do over today, what would it be?",
+            "Did I get enough sleep last night?",
+            "What was the worst part of my day?",
+            "How do I manage time in my day?",
+            "Did I enjoy work today?",
+            "Did I learn something new today?",
+            "What do I remember from the people I've talked to today?"
         };
-        
+
     }
 
     public string GetRandomPrompt()
